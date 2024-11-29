d8987470bf9b0fd1f0f4f9d4d5b0e0ae4996909a
Iryna <irochka.tarasenko2004@stud.op.edu.ua>
2024-11-29 22:27:46 +0200

    Update add.js

--- src/add.js
+++ src/add.js
@@ -1,6 +1,6 @@
 function add(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
-      throw new Error('Аргументи повинні бути числами');
+      throw new Error('Аргументи повинні бути числами.');
     }
     return a + b;
   }
